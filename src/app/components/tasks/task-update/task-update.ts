9d4ea7072638b4e0747a1a683ec37738c9f2ef03
AstroNey <l.nicolas02@laposte.net>
2025-10-29 10:40:56 +0100

    update

---
+++ src/app/components/tasks/task-update/task-update.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-task-update',
+    imports: [],
+    templateUrl: './task-update.html',
+    styleUrl: './task-update.css',
+    standalone: true,
+})
+export class TaskUpdate {
+
+}
